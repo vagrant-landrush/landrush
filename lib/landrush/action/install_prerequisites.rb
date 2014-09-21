823c315068e0d0e1a564c5b6dabba7555962566e
Felix Gilcher <felix.gilcher@asquera.de>
2014-09-21 16:37:51 +0200

    add a guard to wait until machine communication is available
    centos seems to enable ssh later in the boot process, triggering a race condition where landrush
    tries to communicate with the machine before ssh is available. We can guard the execution of the
    landrush capabilities check and wait until machine communication is available.

--- lib/landrush/action/install_prerequisites.rb
+++ lib/landrush/action/install_prerequisites.rb
@@ -10,6 +10,7 @@ module Landrush
       end
 
       def install_prerequisites
+        machine.communicate.wait_for_ready(60)
         unless machine.guest.capability(:iptables_installed)
           info 'iptables not installed, installing it'
           machine.guest.capability(:install_iptables)
