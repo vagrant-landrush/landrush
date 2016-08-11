e1a8062effe1e4439c7374f926846ee286421296
Michael Daley <mjdaley1@ccri.edu>
2016-08-11 12:01:38 -0400

    Vagrant would print error message "The system cannot find the path specified."  Need to detect the path.

--- lib/landrush/cap/host/debian/host.rb
+++ lib/landrush/cap/host/debian/host.rb
@@ -3,6 +3,7 @@ module Landrush
     module Debian
       class DebianHost < Vagrant.plugin('2', 'host')
         def detect?(_env)
+          return false unless Pathname('/etc/issue').exist?
           system("cat /etc/issue | grep 'Debian' > /dev/null 2>&1")
         end
       end
