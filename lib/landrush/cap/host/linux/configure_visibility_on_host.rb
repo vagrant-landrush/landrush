060b0da997661a0911b39bc2853a006b6ec863de
Reto Kaiser <reto@cargomedia.ch>
2016-12-07 14:46:47 +0100

    Fix error message

--- lib/landrush/cap/host/linux/configure_visibility_on_host.rb
+++ lib/landrush/cap/host/linux/configure_visibility_on_host.rb
@@ -8,7 +8,7 @@ module Landrush
             env.host.capability(:create_dnsmasq_config, ip, tld)
             env.host.capability(:restart_dnsmasq)
           rescue Vagrant::Errors::CapabilityNotFound => e
-            env.ui.info("Your host was detected as '#{e.extra_data[:cap]}' for which the host capability " \
+            env.ui.info("Your host was detected as '#{e.extra_data[:host]}' for which the host capability " \
             "'#{e.extra_data[:cap]}' is not available.")
             env.ui.info('Check the documentation for the manual instructions to configure the visibility on the host.')
           end
